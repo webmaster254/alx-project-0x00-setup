f8349b7796f3e4dd5c0ff834e107eb3d417da942
webmaster254 <josephm2800@gmail.com>
2025-06-01 22:44:16 +0300

    feat: add basic landing and about page components with minimal styling

---
+++ alx-project-0x00/pages/about.tsx
@@ -0,0 +1,9 @@
+const About: React.FC = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-extralight">About</h1>
+        </div>
+    );
+};
+
+export default About;
---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,9 @@
+const Landing: React.FC = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-extralight">Landing</h1>
+        </div>
+    );
+};
+
+export default Landing;
