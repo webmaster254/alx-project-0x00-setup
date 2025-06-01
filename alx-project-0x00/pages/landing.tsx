f5e15bc4d307009a6488f1830bec872bb6ebdc71
webmaster254 <josephm2800@gmail.com>
2025-06-01 22:54:59 +0300

    feat: add Card component to landing page and reorganize image assets

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,7 +1,11 @@
+
+import Card from "../components/Card";
+
 const Landing: React.FC = () => {
     return (
         <div>
             <h1 className="text-3xl font-extralight">Landing</h1>
+            <Card />
         </div>
     );
 };
